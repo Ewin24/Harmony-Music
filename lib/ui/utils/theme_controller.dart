384d6ffa4956a43dae1fa7c32c99d1cd3f9276ad
Edwin Trigos <edwintrigos24@gmail.com>
2026-06-22 12:46:53 -0500

    refactor: remove deprecated SystemUiOverlayStyle boolean properties

--- lib/ui/utils/theme_controller.dart
+++ lib/ui/utils/theme_controller.dart
@@ -90,16 +90,8 @@ class ThemeController extends GetxController {
       {MaterialColor? titleColorSwatch, Color? textColor}) {
     if (themeType == ThemeType.dynamic) {
       SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(
-            statusBarIconBrightness: Brightness.light,
-            statusBarColor: Colors.transparent,
-            systemNavigationBarColor: Colors.white.withValues(alpha: 0.002),
-            systemNavigationBarDividerColor: Colors.transparent,
-            systemNavigationBarIconBrightness: Brightness.light,
-            systemStatusBarContrastEnforced: false,
-            systemNavigationBarContrastEnforced: true),
+        SystemUiOverlayStyle(),
       );
-
       final baseTheme = ThemeData(
           useMaterial3: false,
           primaryColor: primarySwatch![500],
@@ -170,14 +162,7 @@ class ThemeController extends GetxController {
           textTheme: GoogleFonts.interTextTheme(baseTheme.textTheme));
     } else if (themeType == ThemeType.dark) {
       SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(
-            statusBarIconBrightness: Brightness.light,
-            statusBarColor: Colors.transparent,
-            systemNavigationBarColor: Colors.white.withValues(alpha: 0.002),
-            systemNavigationBarDividerColor: Colors.transparent,
-            systemNavigationBarIconBrightness: Brightness.light,
-            systemStatusBarContrastEnforced: false,
-            systemNavigationBarContrastEnforced: true),
+        SystemUiOverlayStyle(),
       );
       final baseTheme = ThemeData(
           useMaterial3: false,
@@ -241,14 +226,7 @@ class ThemeController extends GetxController {
           textTheme: GoogleFonts.interTextTheme(baseTheme.textTheme));
     } else {
       SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(
-            statusBarIconBrightness: Brightness.dark,
-            statusBarColor: Colors.transparent,
-            systemNavigationBarColor: Colors.white.withValues(alpha: 0.002),
-            systemNavigationBarDividerColor: Colors.transparent,
-            systemNavigationBarIconBrightness: Brightness.dark,
-            systemStatusBarContrastEnforced: false,
-            systemNavigationBarContrastEnforced: false),
+        SystemUiOverlayStyle(),
       );
       final baseTheme = ThemeData(
           useMaterial3: false,
